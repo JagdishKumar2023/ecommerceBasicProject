0e7b48dc7058bc5397a5c296ee2ec994e7be6b98
JagdishKumar2023 <jagdishkumarsingh2023@gmail.com>
2024-03-29 14:10:20 +0530

    New CHanesges in createContext

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,23 +1,17 @@
 import React, { useContext, useState } from "react";
 import "./Searchbar.css";
-import ecommData from "../ProductData/ProductData";
 import { IoIosSearch } from "react-icons/io";
 import { SearchContext } from "../Context/SearchContext";
 // import { SearchContext } from "../Context/SearchContext";
 
 const Searchbar = () => {
-  const {
-    searchData,
-    setSerachData,
-    handleChange,
-    handleSearch,
-    handleEnterKey,
-  } = useContext(SearchContext);
+  const { searchData, handleChange, handleSearch, handleEnterKey } =
+    useContext(SearchContext);
   return (
     <div className="searchContainer">
       <input
         type="search"
-        placeholder="eg. shirt, shoes, watch..."
+        placeholder="eg. shirt, shoes, Jeans ,watch..."
         className="inputBox"
         value={searchData}
         onChange={handleChange}
